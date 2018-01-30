a8249a2711b033f06a76087aa3565c8a78a1d953
Grosz, Logan <Logan.Grosz@mines.sdsmt.edu>
2018-01-30 09:09:28 -0700

    Added Psuedo code

---
+++ CSV2LaTeX/CSV2LaTeX.cpp
@@ -0,0 +1,20 @@
+/*
+* Psuedocode:
+* 
+* Command line arugments: input file (csv), and include headers?
+* Read amount of rows to determine size
+*	keep track of the columns
+* Dynamically allocate a 2D array of size row,col
+* Check to made sure it was made
+* Seekg to front of file and begin reading into array
+*	(maybe get line then split into tokens by commas?)
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
